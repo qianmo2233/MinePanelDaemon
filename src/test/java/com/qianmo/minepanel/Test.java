66570873b0d73400fb30132d77ee0627e0797710
qianmo <67282958@qq.com>
2021-06-19 17:25:49 +0800

    dev 0.0.8

---
+++ src/test/java/com/qianmo/minepanel/Test.java
@@ -0,0 +1,11 @@
+package com.qianmo.minepanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tests {
+    @Test
+    void contextLoads() {
+    }
+}
